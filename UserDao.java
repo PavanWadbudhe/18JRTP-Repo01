4b3ca5a350d745182b0a0efe9852d57173429192
Pavan <pavanwadbudhe1998@gmail.com>
2024-03-20 22:20:13 +0530

    UserDao updated

--- UserDao.java
+++ UserDao.java
@@ -1,4 +1,11 @@
 public class UserDao{
-  
+
+  public void message(){
+     System.out.println("Welcome...");
+  }
+  public static void main(String[] args){
+    UserDao dao=new UserDao();
+    dao.message;
+  }
 
 }
